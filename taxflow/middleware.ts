4a3d2214f3f432ead9f5636eb71f25e353805723
Gomez <gomezopeyemi@gmail.com>
2026-04-26 14:38:08 +0100

    taxflow/middleware.ts

--- taxflow/middleware.ts
+++ taxflow/middleware.ts
@@ -73,10 +73,3 @@ export const config = {
     "/((?!_next/static|_next/image|favicon.ico|public).*)",
   ],
 };
-}
-
-export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon\\.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};
